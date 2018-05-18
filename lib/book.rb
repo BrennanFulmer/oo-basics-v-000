c1d58d055cf6d427188cdd739d3c512982def0c0
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-18 02:19:54 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
 class Book
+    attr_accessor(:title, :author, :page_count, :genre)
+    
   def initialize(title)
     title=(title)
   end
 
-  attr_accessor(:title, :author, :page_count, :genre)
+
   
 
 end
